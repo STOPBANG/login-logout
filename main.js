cacbc564b6c48b70679b8129348c97f5d7f9a453
ywonchae1 <ywonchae62@gmail.com>
2024-04-04 01:10:33 +0900

    fix: 불필요한 코드 제거

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-const jwt = require("jsonwebtoken");
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
@@ -10,30 +9,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET_KEY));
 
-const layouts = require("express-ejs-layouts");
-app.set("view engine", "ejs");
-app.use(layouts);
 app.set("port", process.env.PORT || 3000);
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-app.use((req, res, next) => {
-    try {
-      const decoded = jwt.verify(
-        req.cookies.authToken,
-        process.env.JWT_SECRET_KEY
-      );
-      res.locals.auth = decoded.userId;
-      res.locals.userType = req.cookies.userType;
-      res.locals.is_admin = adminControl.getAdmin(decoded.userId);
-    } catch (error) {
-      res.locals.auth = "";
-      res.locals.userType = "";
-      res.locals.is_admin = "";
-    }
-    next();
-});
 
 const authController = require("./controllers/authController");
 
