4f71f83e1904dfc5c8ae6309d6312ffb1ce5be11
ywonchae1 <ywonchae62@gmail.com>
2024-04-25 10:35:59 +0900

    fix: 로그인 비밀번호 검사 추가, 로그인 정보 없는 경우 오류 해결

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,15 @@
 const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt');
 const {httpRequest} = require('../utils/httpRequest');
+
+const pwCompare = async (input, pwHash) => {
+  try {
+    const res = await bcrypt.compare(input, pwHash);
+    return res;
+  } catch(err) {
+    return false;
+  }
+}
 
 module.exports = {
   login: async (req, res) => {
@@ -32,37 +42,46 @@ module.exports = {
           return res; // 있다면 다음 콜백으로
         }
       })
-      .then((response) => {
+      .then(async (response) => {
         const body = response.body[0];
-        console.log(body);
-        if(body.r_username) { // 정보가 있다면 쿠키 생성
-          const token = jwt.sign({userId: body.r_username}, process.env.JWT_SECRET_KEY);
-          res.cookie("authToken", token, {
-            maxAge: 86400_000,
-            httpOnly: true,
-          });
-          res.cookie("userType", 1, { // 입주민
-            maxAge: 86400_000,
-            httpOnly: true,
-          });
-          return res.redirect('/');
-        } else if(body.a_username) {
-          const token = jwt.sign({userId: body.a_username}, process.env.JWT_SECRET_KEY);
-          res.cookie("authToken", token, {
-            maxAge: 86400_000,
-            httpOnly: true,
-          });
-          res.cookie("userType", 0, { // 공인중개사
-            maxAge: 86400_000,
-            httpOnly: true,
-          });
-          return res.redirect('/');
-        } else {
+        if(!body) {
           res.cookie('authToken', null, {
             maxAge: 86400_000,
             httpOnly: true,
           });
           return res.redirect('/auth/login'); // 정보가 없다면 리다이렉트
+        } else if(body.r_username) { // 정보가 있다면 쿠키 생성
+          // 비밀번호 일치 확인
+          const result = await pwCompare(requestBody.password, body.r_password)
+          if(!result) return res.redirect('/auth/login');
+          else {
+            const token = jwt.sign({userId: body.r_username}, process.env.JWT_SECRET_KEY);
+            res.cookie("authToken", token, {
+              maxAge: 86400_000,
+              httpOnly: true,
+            });
+            res.cookie("userType", 1, { // 입주민
+              maxAge: 86400_000,
+              httpOnly: true,
+            });
+            return res.redirect('/');
+          }
+        } else if(body.a_username) {
+          // 비밀번호 일치 확인
+          const result = await pwCompare(requestBody.password, body.r_password)
+          if(!result) return res.redirect('/auth/login');
+          else {
+            const token = jwt.sign({userId: body.a_username}, process.env.JWT_SECRET_KEY);
+            res.cookie("authToken", token, {
+              maxAge: 86400_000,
+              httpOnly: true,
+            });
+            res.cookie("userType", 0, { // 공인중개사
+              maxAge: 86400_000,
+              httpOnly: true,
+            });
+            return res.redirect('/');
+          }
         }
       });
   },
